26e3c4b3f0fd205d07d898f0f2ea7443b948b308
rajbi <106402644+rajbi1380@users.noreply.github.com>
2022-09-09 14:42:48 +0430

    Delete keyboard directory

--- keyboard/key.js
+++
@@ -1,9 +0,0 @@
-// const item2 = document.querySelector(".keyboard");
-const i1 = document.querySelectorAll(".item2");
-
-i1.addEventListener("keydown", function (event) {
-  if (event.code == "KeyA" && event.ctrlKey) {
-    console.log("a");
-    // i1.classList.toggle("active");
-  }
-});
